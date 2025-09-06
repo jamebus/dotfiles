d9316aabad152e13ee121a5fb4cbdbdb2219c853
James Kelly <james@hicag.org>
2025-09-06 11:32:48 -0500

    Add util.dot and util.mini-hipatterns LazyExtras

--- dot_config/nvim/lua/extras/extras.lua
+++ dot_config/nvim/lua/extras/extras.lua
@@ -14,4 +14,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.toml" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.util.chezmoi" },
+  { import = "lazyvim.plugins.extras.util.dot" },
+  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 }
