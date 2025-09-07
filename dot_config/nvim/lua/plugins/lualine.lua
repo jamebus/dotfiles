71c0b641f0ab925dbe221880d3b7950a6e1fec3e
James Kelly <james@hicag.org>
2025-09-07 11:13:42 -0500

    Add nvim lualine config

---
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      component_separators = "",
+      section_separators = "",
+    },
+    sections = {
+      lualine_z = { "lsp_status" },
+    },
+  },
+}
