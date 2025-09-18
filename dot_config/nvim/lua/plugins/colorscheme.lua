d29b83e100cb63180e553c87d38d0306e273ebbc
James Kelly <james@hicag.org>
2025-09-17 19:27:59 -0500

    Remove workaround for LazyVim/LazyVim/issues/6355
    LazyVim v15.0.0, just released, includes a fix for this

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -19,17 +19,4 @@ return {
       show_end_of_buffer = true,
     },
   },
-
-  -- bug: failed to run config for bufferline.nvim
-  -- https://github.com/LazyVim/LazyVim/issues/6355
-  {
-    "catppuccin/nvim",
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
-  },
 }
