5e8f38adcb8ff41874425044f1dcb03765d52fd9
James Kelly <james@hicag.org>
2025-11-02 18:36:14 -0600

    Add yanky to neovim

--- dot_config/nvim/lua/extras/extras.lua
+++ dot_config/nvim/lua/extras/extras.lua
@@ -1,6 +1,7 @@
 return {
   { import = "lazyvim.plugins.extras.coding.mini-comment" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
   { import = "lazyvim.plugins.extras.lang.ansible" },
   { import = "lazyvim.plugins.extras.lang.docker" },
