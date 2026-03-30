e0d89e69b7a3fdf5bcb5a5fb913c039c3e48ff34
James Kelly <james@hicag.org>
2026-03-30 11:14:22 -0500

    neovim: allow mise plugin to be optional

--- dot_config/nvim/lua/plugins/mise.lua
+++ dot_config/nvim/lua/plugins/mise.lua
@@ -1,10 +1,14 @@
-return {
-  {
-    "ejrichards/mise.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-      "mason-org/mason.nvim",
+if vim.fn.executable("mise") == 1 then
+  return {
+    {
+      "ejrichards/mise.nvim",
+      event = "VeryLazy",
+      opts = {},
+      dependencies = {
+        "mason-org/mason.nvim",
+      },
     },
-  },
-}
+  }
+else
+  return {}
+end
